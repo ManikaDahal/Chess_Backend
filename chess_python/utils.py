c397e1f5f23b63d9589510cc8dac39082a686265
Manika Dahal <manikadahal8@gmail.com>
2026-01-12 11:07:54 +0545

    Commit befor deploying

--- chess_python/utils.py
+++ chess_python/utils.py
@@ -3,10 +3,15 @@ from django.conf import settings
 import random
 
 
-def send_sms(to_phone:str,message:str):
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    client.messages.create(
-        body=message,
-        from_=settings.TWILIO_PHONE_NUMBER,
-        to=to_phone
-    )
+def send_sms(to_phone: str, message: str):
+    try:
+        client = Client(settings.TWILIO_SID, settings.TWILIO_AUTH_TOKEN)
+        msg = client.messages.create(
+            body=message,
+            from_=settings.TWILIO_PHONE,
+            to=to_phone
+        )
+        print(f"SMS sent: SID={msg.sid}")  # This will print in your Django terminal
+    except Exception as e:
+        print(f"Twilio Error: {str(e)}")
+        raise
