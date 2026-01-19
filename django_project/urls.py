3e4106a7e5ed4b5ca0f318ddfbdb5850707a29df
Manika Dahal <manikadahal8@gmail.com>
2026-01-19 07:57:28 +0545

    Fixed jwt token problem

--- django_project/urls.py
+++ django_project/urls.py
@@ -45,7 +45,7 @@ schema_view=get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chess_python.urls')),
-    path('api/token/', TokenObtainPairView.as_as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     # CHANGE: Added Swagger documentation URLs
