c12ae936d633d8d2a3fcbd2b9eaefad6ea53c518
Manika Dahal <manikadahal8@gmail.com>
2026-01-22 19:31:54 +0545

    Added admin

--- chess_python/admin.py
+++ chess_python/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser, PasswordResetOTP
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'phone', 'is_staff', 'date_joined')
+    search_fields = ('username', 'email', 'phone')
+
+@admin.register(PasswordResetOTP)
+class PasswordResetOTPAdmin(admin.ModelAdmin):
+    list_display = ('user', 'otp', 'created_at')
