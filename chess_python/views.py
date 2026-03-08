1654c09a700e6566c6b2450c5f1e5213729b0e2f
Manika Dahal <manikadahal8@gmail.com>
2026-03-08 12:42:35 +0545

    Added missimg import

--- chess_python/views.py
+++ chess_python/views.py
@@ -10,6 +10,7 @@ from .models import PasswordResetOTP, FCMToken
 from datetime import timedelta
 from django.utils import timezone
 from .utils import send_sms
+from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import SignupSerializer, ForgotPasswordSerializer, VerifyOTPSerializer, ResetPasswordSerializer, FCMTokenSerializer, EmailTokenObtainPairSerializer
 from drf_yasg.utils import swagger_auto_schema
