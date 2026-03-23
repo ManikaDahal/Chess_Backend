f1150927fe4f95495c22c9337388707c473e7672
Manika Dahal <manikadahal8@gmail.com>
2026-03-23 07:36:30 +0545

    Fixing render deployment issue

--- django_project/asgi.py
+++ django_project/asgi.py
@@ -8,16 +8,17 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+django_application = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import call.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_application,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             call.routing.websocket_urlpatterns
